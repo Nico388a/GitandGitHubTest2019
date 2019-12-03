ee36f464ba80e788d3aba28fd379d774bb55b1f9
Tristan Taylor Kamp <tris0032@edu.easj.dk>
2019-12-03 10:28:24 +0100

    Tristan tilføjede CW

--- GitandGitHubTest2019/Program.cs
+++ GitandGitHubTest2019/Program.cs
@@ -12,7 +12,8 @@ namespace GitandGitHubTest2019
         {
             //Dette er lavet lokalt
             Console.WriteLine("Dette er en udskrivning lavet lokalt");
-            /Dette er en kommentar der er blevet lavet på serveren
+            //Dette er en kommentar der er blevet lavet på serveren
+            Console.WriteLine("Hej Nicolaj, dette her må være den langsommeste chat nogensinde");
             Console.ReadLine();
         }
     }
